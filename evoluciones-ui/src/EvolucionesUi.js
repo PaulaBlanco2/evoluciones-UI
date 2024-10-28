8c3ab0e8538b0f5fba827412d6cb0189fe25d3ba
Pau <blancopaula48@gmail.com>
2024-10-28 17:40:26 -0500

    integracion dm

--- evoluciones-ui/src/EvolucionesUi.js
+++ evoluciones-ui/src/EvolucionesUi.js
@@ -3,6 +3,8 @@ import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-hel
 import styles from './evoluciones-ui.css.js';
 import '@bbva-experience-components/bbva-button-default/bbva-button-default.js';
 import '@bbva-experience-components/bbva-progress-content/bbva-progress-content.js';
+import '@pokedex/pokemones-dm/pokemones-dm.js'
+
 
 export class EvolucionesUi extends LitElement {
   static get properties() {
@@ -22,58 +24,7 @@ export class EvolucionesUi extends LitElement {
     this.evolutions = [];
     this.noEvolutionsMessage = '';
     this.loading = true; 
-  }
-
-  async fetchPokemonDetails() {
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonId}/`);
-      const data = await response.json();
-      this.pokemonDetails = {
-        name: data.name,
-        image: data.sprites.front_default,
-        types: data.types.map(typeInfo => typeInfo.type.name).join(', '),
-      };
-
-      const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.pokemonId}/`);
-      const speciesData = await speciesResponse.json();
-      const evolutionChainUrl = speciesData.evolution_chain.url;
-
-      const evolutionResponse = await fetch(evolutionChainUrl);
-      const evolutionData = await evolutionResponse.json();
-      this.evolutions = await this.extractEvolutionsWithImages(evolutionData.chain);
-
-      this.noEvolutionsMessage = this.evolutions.length === 0
-        ? 'Este Pokémon no tiene evoluciones.'
-        : '';
-    } catch (error) {
-      console.error('Error al obtener detalles del Pokémon:', error);
-    }
-  }
-
-  async extractEvolutionsWithImages(chain) {
-    const evolutions = [];
-    let current = chain;
-
-    while (current) {
-      const evolutionName = current.species.name;
-      const evolutionImage = await this.fetchPokemonImage(evolutionName);
-      evolutions.push({ name: evolutionName, image: evolutionImage });
-      current = current.evolves_to[0];
-    }
-
-    return evolutions;
-  }
-
-  async fetchPokemonImage(evolutionName) {
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${evolutionName}/`);
-      const data = await response.json();
-      return data.sprites.front_default;
-    } catch (error) {
-      console.error(`Error al obtener imagen de ${evolutionName}:`, error);
-      return '';
-    }
-  }
+  }  
 
   static get styles() {
     return [
@@ -82,16 +33,29 @@ export class EvolucionesUi extends LitElement {
     ];
   }
 
+  firstUpdated() {
+    this.addEventListener('pokemon-details-loaded', (e) => {
+      this.pokemonDetails = e.detail.pokemonDetails;
+      this.evolutions = e.detail.evolutions;
+      this.noEvolutionsMessage = e.detail.noEvolutionsMessage;
+      this.loading = false;
+    });
+
+    const pokemonesDm = this.shadowRoot.querySelector('pokemones-dm');
+    if (pokemonesDm) {
+      pokemonesDm.fetchPokemonDetails(this.pokemonId);
+    }
+  }
+
   render() {
     return html`
-      ${this.loading
-        ? html`
-            <bbva-progress-content
-              accessibility-text="bbva-progress-content-login-loading-message"
+      <div class="main-container">
+        ${this.loading
+          ? html`<bbva-progress-content
+              accessibility-text="bbva-progress-content-loading"
               visible=""
-            ></bbva-progress-content>
-          `
-        : html`
+            ></bbva-progress-content>`
+          : html`
             <div class="pokemon-card">
               <h1 class="pokemon-name">${this.pokemonDetails.name}</h1>
               <img src="${this.pokemonDetails.image}" alt="${this.pokemonDetails.name}" class="pokemon-image" />
@@ -115,19 +79,14 @@ export class EvolucionesUi extends LitElement {
               <bbva-button-default @click="${this.goTopokemon}">Ver Pokemones</bbva-button-default>
             </div>
           `}
+      </div>
+      <pokemones-dm></pokemones-dm>
     `;
   }
 
-  firstUpdated() {
-    this.fetchPokemonDetails();
-    setTimeout(() => {
-      this.loading = false; 
-    }, 2000);
-  }
 
   goTopokemon() {
     this.navigate('pokemon');
   }
 }
 
-customElements.define('evoluciones-ui', EvolucionesUi);
